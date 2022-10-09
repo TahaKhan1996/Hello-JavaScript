8bdd090ef8796323903727f3d50243ea964ce7fe
TahaKhan1996 <115425232+TahaKhan1996@users.noreply.github.com>
2022-10-09 12:26:47 -0700

    Add files via upload
    Hello World javascript

---
+++ Taha project.js
@@ -0,0 +1,20 @@
+// 1. Ways to print in JavaScript
+ console.log("Hello world");
+alert("me");
+document.write("this is document write")
+
+//2.  JavaScript console API 
+//console.log("Hello wolrd", 4 + 6, "Another log");
+// console.warn(this is warning");
+// console.error("this is error");
+
+//3.  JavaScript Variables
+//what are variables? - Container to store data values
+
+/*
+multi
+line
+commment
+*/
+
+var number1 = 34;
